4456190087d03f5bd0bf2871a4c56167ab1ac5f2
lihong <khut.lihong@gmail.com>
2025-09-27 15:56:30 +0700

    refactor: Clean up imports and remove unused MainMenu component from HomeComponent

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,22 @@
+import HomeHeader from "@/components/Pages/HomePage/HomeHeader";
+import OffersCarousel from "@/components/Pages/HomePage/OffersCarousel";
+import ServiceGrid from "@/components/Pages/HomePage/ServiceGrid";
+import { createFileRoute } from "@tanstack/react-router";
 
-import HomeHeader from '@/components/Pages/HomePage/HomeHeader';
-import OffersCarousel from '@/components/Pages/HomePage/OffersCarousel';
-import ServiceGrid from '@/components/Pages/HomePage/ServiceGrid';
-import MainMenu from '@/components/MainMenu'
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: HomeComponent,
-})
+});
 
 function HomeComponent() {
   return (
-    <div className="flex flex-col justify-center w-full max-w-md min-h-screen mx-auto bg-muted-background">
+    <div className="flex flex-col justify-center w-full max-w-md mx-auto bg-muted-background">
       <HomeHeader />
-      <MainMenu />
       <div className="pt-6 space-y-6">
         <ServiceGrid />
         <OffersCarousel />
       </div>
 
-      <div className='pb-8' />
+      <div className="pb-8" />
     </div>
   );
 }
