560b16dcdfe6f34e8c515b464f56d835aa233fe5
BunhengG <bunheng12pro@gmail.com>
2025-10-17 11:49:17 +0700

    Updated code in HeaderComponent to handle passed dynamic service_type into Support Route.

--- src/components/layout/HeaderComponent.tsx
+++ src/components/layout/HeaderComponent.tsx
@@ -1,31 +1,63 @@
 "use client";
 
 import { useRouter } from "@tanstack/react-router";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, PhoneCallIcon } from "lucide-react";
+import { Button } from "../ui/button";
 
 interface PageHeaderProps {
   title?: string;
   showBack?: boolean;
-  iconBgColor?: string;
+  showContact?: boolean;
+  onContactClick?: () => void;
   headerBgColor?: string;
   noBorder?: boolean;
 }
 
-export default function HeaderComponent({ title, showBack = true, iconBgColor, headerBgColor, noBorder }: PageHeaderProps) {
+export default function HeaderComponent({
+  title,
+  showBack = true,
+  showContact = false,
+  onContactClick,
+  headerBgColor,
+  noBorder,
+}: PageHeaderProps) {
   const router = useRouter();
 
   return (
-    <header className={`fixed inset-x-0 top-0 z-50 ${headerBgColor ? headerBgColor : "bg-muted-background"} ${noBorder ? "" : "border-b border-gray-200"}`}>
+    <header
+      className={`fixed inset-x-0 top-0 z-50 ${headerBgColor ? headerBgColor : "bg-muted-background"} ${noBorder ? "" : "border-b border-gray-200"}`}
+    >
       <div className="relative flex items-center justify-center w-full max-w-md px-4 py-4 mx-auto text-accent-foreground">
-        {showBack && (
-          <button
-            onClick={() => router.history.back()}
-            className={`absolute left-5 rounded-lg cursor-pointer h-8 w-8 flex items-center justify-center ${iconBgColor ? iconBgColor : "bg-base-primary"}`}
-            aria-label="Go back">
-            <ChevronLeft className="!text-white" size={24} />
-          </button>
+        <div className="absolute left-4">
+          {showBack && (
+            <Button
+              onClick={() => router.history.back()}
+              size="icon"
+              aria-label="Go back"
+              className="w-8 h-8 rounded-lg bg-base-primary"
+            >
+              <ChevronLeft className="!text-white" size={20} />
+            </Button>
+          )}
+        </div>
+        {title && (
+          <h2 className="w-full px-4 text-lg font-bold text-center truncate">
+            {title}
+          </h2>
         )}
-        {title && <h2 className="w-full px-8 text-lg font-bold text-center truncate">{title}</h2>}
+
+        <div className="absolute right-4">
+          {showContact && (
+            <Button
+              onClick={onContactClick}
+              size="icon"
+              aria-label="Contact"
+              className="w-8 h-8 rounded-lg bg-blue-500/25"
+            >
+              <PhoneCallIcon className="!text-blue-500" size={20} />
+            </Button>
+          )}
+        </div>
       </div>
     </header>
   );
