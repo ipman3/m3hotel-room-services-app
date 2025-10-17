d2304cbbb9a5d49535477b2ff896d34b698dc75e
BunhengG <bunheng12pro@gmail.com>
2025-10-17 11:55:05 +0700

    Added New route which is Orders

--- src/routes/orders/index.tsx
+++ src/routes/orders/index.tsx
@@ -1,3 +1,6 @@
+import HeaderComponent from '@/components/layout/HeaderComponent';
+import MainPage from '@/components/Pages/OrdersPage/MainPage';
+import { useOrderStore } from '@/store/CartStore';
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/orders/')({
@@ -5,5 +8,13 @@ export const Route = createFileRoute('/orders/')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/orders/"!</div>
+  const orderItems = useOrderStore((state) => state.items);
+  return (
+    <div>
+      <HeaderComponent title="View Order" showBack={false} />
+      <main className="mt-12">
+        <MainPage items={orderItems} />
+      </main>
+    </div>
+  );
 }
