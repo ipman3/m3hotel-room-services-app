4b2e4a9925116113e09a5a2656acfcb48a565f79
BunhengG <bunheng12pro@gmail.com>
2025-10-20 08:40:42 +0700

    Add TackStack Query DevTool

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { createRootRoute } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import LoadingProvider from '@/context/LoadingContext';
 import AppLayout from '@/components/layout/AppLayout';
 
@@ -15,6 +16,7 @@ function RootComponent() {
       <LoadingProvider>
         <AppLayout />
       </LoadingProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
